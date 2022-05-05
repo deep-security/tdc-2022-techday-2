8400d298ac552fc286ff8eef99cead1f7b049b4f
Alex Jackson <ajaxbits@pm.me>
2022-05-05 15:48:11 +0000

    gate watch functionality of watch.sh behind a watch flag

--- utils/vend.sh
+++ utils/vend.sh
@@ -18,7 +18,9 @@ then
     echo "Github CLI could not be found. Install it here: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html"
     exit
 fi
+echo "Checking Github authentication status..."
 gh auth status #returns exit code 1 if user not logged in
+echo ""
 
 # Set variables
 LATESTUPSTREAMHASH=$(git rev-parse '@{u}')
@@ -33,19 +35,28 @@ then
         -f name="$(git rev-parse '@{u}')"
 fi
 
-# Watch execution
-sleep 3
-gh run watch --exit-status # will exit with error code 1 if run fails
+# Define the Vending Machine deploy action
+function watch-action {
+    gh run watch --exit-status # will exit with error code 1 if run fails
 
-# Once run is complete, get the job ID
-echo "Getting job ID"
-RUNJSON=$(gh run list --workflow="$DEPLOYWORKFLOWNAME" \
-    --branch "$CURRENTBRANCHNAME" \
-    --json headSha,databaseId \
-    -q ".[] | select(.headSha == \"$LATESTUPSTREAMHASH\")")
-RUNID=$(echo "$RUNJSON" | jq '.databaseId')
-JOBID=$(gh run view "$RUNID" | grep "gh run view" | awk '{print $11}' | tail -c 11)
+    # Once run is complete, get the job ID
+    echo "Getting job ID"
+    RUNJSON=$(gh run list --workflow="$DEPLOYWORKFLOWNAME" \
+        --branch "$CURRENTBRANCHNAME" \
+        --json headSha,databaseId \
+        -q ".[] | select(.headSha == \"$LATESTUPSTREAMHASH\")")
+    RUNID=$(echo "$RUNJSON" | jq '.databaseId')
+    JOBID=$(gh run view "$RUNID" | grep "gh run view" | awk '{print $11}' | tail -c 11)
 
-# Get the json from the logs of the vending job
-echo "Credentials are:"
-gh run view --log --job "$JOBID" | awk 'BEGIN { first = 7; last = 100 }{ for (i = first; i < last; i++) { printf("%s ", $i) } print $last }' | grep -A11 -B1 "PlayerUserName" | jq
+    # Get the json from the logs of the vending job
+    echo "Credentials are:"
+    gh run view --log --job "$JOBID" | awk 'BEGIN { first = 7; last = 100 }{ for (i = first; i < last; i++) { printf("%s ", $i) } print $last }' | grep -A11 -B1 "PlayerUserName" | jq
+}
+
+# Handle the watch options
+while true; do
+  case "$1" in
+    -w | --watch ) watch-action; exit ;;
+    * ) exit ;;
+  esac
+done
