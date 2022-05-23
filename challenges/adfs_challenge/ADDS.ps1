cfdb37284f17d870db1881a67a3d6a8b4e381cc5
Jacob Lake <jacob_lake@trendmicro.com>
2022-05-23 17:29:21 -0400

    add PS scripts

---
+++ challenges/adfs_challenge/ADDS.ps1
@@ -0,0 +1,7 @@
+#install Domain Services for Domain Controller
+$domainName = "techday.com"
+$password = "TrendMicro123!"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+ 
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword&amp;amp;amp;nbsp;$securePassword&amp;amp;amp;nbsp;-Force
