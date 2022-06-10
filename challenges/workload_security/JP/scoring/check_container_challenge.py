c8eca1814237ab841fd8476873d6ab0a44da1932
ryoma <82663636+ryomak1985@users.noreply.github.com>
2022-06-10 20:41:30 +0900

    Create check_container_challenge.py
    Checking if the answer is correct, then uploading a file to S3

---
+++ challenges/workload_security/JP/scoring/check_container_challenge.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+#Print when loading function
+print('Loading function')
+def lambda_handler(event, context):
+    s3 = boto3.client("s3", region_name='us-east-1')
+    correct_answer_container = "1005402 - Identified Suspicious User Agent In HTTP Request"
+    print(event)
+    keyword = event[0]["Reason"]
+    print(keyword)
+    if keyword == correct_answer_container:
+        response = s3.put_object(
+        Bucket='ryoma2022', //need to change
+        Key="container_correct",  
+         )
+    else:
+        print("False")
