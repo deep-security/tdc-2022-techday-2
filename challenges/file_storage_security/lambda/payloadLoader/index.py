158f81916580918b87e555de15f0a6355c5cc3c8
Alex Jackson <contact@ajaxbits.com>
2022-06-24 20:43:45 +0000

    init exploitcheck

---
+++ challenges/file_storage_security/lambda/payloadLoader/index.py
@@ -0,0 +1,53 @@
+import boto3
+import json
+import os
+import urllib
+import logging
+from botocore.config import Config
+
+# Values passed in from cfn
+bucket = "${QSS3BucketName}"
+image_bucket = "${ImageUploaderS3Bucket}"
+true_prefix = "${LambdaPrefix}payloadLoader/true.js"
+false_prefix = "${LambdaPrefix}payloadLoader/false.js"
+region = "${AWS::Region}"
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client("s3")
+
+
+def handler(event, context):
+    true_file = "/tmp/true.js"
+    false_file = "/tmp/false.js"
+    try:
+        key = "connectioncheck"
+        signed_url = s3.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": image_bucket, "Key": key},
+            ExpiresIn=500,
+        )
+        res = urllib.request.urlopen(
+            urllib.request.Request(url=signed_url, method="GET"), timeout=5
+        )
+        logger.info(res.status)
+        s3.download_file(bucket, true_prefix, true_file)
+        with open(true_file, "rb") as true_file:
+            data: str = true_file.read().decode("utf-8")
+            data = "const exploit = '/profile/getimg/connectioncheck'\n" + data
+    except:
+        s3.download_file(bucket, false_prefix, false_file)
+        with open(false_file, "rb") as false_file:
+            data = false_file.read().decode("utf-8")
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+            "content-type": "application/javascript",
+        },
+        "body": data,
+    }
