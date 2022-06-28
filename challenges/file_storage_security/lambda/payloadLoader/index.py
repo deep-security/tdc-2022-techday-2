607fe4382b3114b2421d5288e421905747e4fb5a
Alex Jackson <contact@ajaxbits.com>
2022-06-28 22:58:06 +0000

    remove redundant code from exploitcheck

--- challenges/file_storage_security/lambda/payloadLoader/index.py
+++ challenges/file_storage_security/lambda/payloadLoader/index.py
@@ -20,16 +20,6 @@ s3 = boto3.client("s3")
 
 def handler(event, context):
     true_file = "/tmp/true.js"
-    key = "connectioncheck"
-    signed_url = s3.generate_presigned_url(
-        "get_object",
-        Params={"Bucket": image_bucket, "Key": key},
-        ExpiresIn=500,
-    )
-    res = urllib.request.urlopen(
-        urllib.request.Request(url=signed_url, method="GET"), timeout=5
-    )
-    logger.info(res.status)
     s3.download_file(bucket, true_prefix, true_file)
     with open(true_file, "rb") as true_file:
         data: str = true_file.read().decode("utf-8")
