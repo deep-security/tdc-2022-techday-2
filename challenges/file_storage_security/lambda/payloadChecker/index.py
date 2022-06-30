41d74b7131d6690745e1f97182d4817c37cb08cb
Alex Jackson <contact@ajaxbits.com>
2022-06-30 04:29:41 +0000

    fix payload checker to actually compare file contents

--- challenges/file_storage_security/lambda/payloadChecker/index.py
+++ challenges/file_storage_security/lambda/payloadChecker/index.py
@@ -1,15 +1,37 @@
 import boto3
+import filecmp
+import zipfile
+import urllib.request
 
-bucket = "${S3BucketResources.Outputs.ImageUploaderS3BucketName}"
+
+bucket = "${ImageUploaderS3Bucket}"
+pwned_url = "https://${ToolsPrefix}pwned.zip"
 file_key = "connectioncheck"
-s3_client = boto3.client("s3")
+
+s3 = boto3.client("s3")
+
+
+def get_payload():
+    download_location = "/tmp/pwned.zip"
+    with open(download_location, "wb") as control_file:
+        res = urllib.request.urlopen(
+            urllib.request.Request(url=pwned_url, method="GET"), timeout=5
+        )
+        control_file.write(res.read())
+
+    with zipfile.ZipFile(download_location, "r") as zip_ref:
+        zip_ref.extractall(path="/tmp", pwd=b"novirus")
+    return "/tmp/connectioncheck"
 
 
 def handler(event, context):
-    result = s3_client.list_objects_v2(Bucket=bucket, Prefix=file_key)
-    print(result)
-    if "Contents" in result:
-        print("You've hacked the service!")
-        return True
-    else:
+    player_file = "/tmp/player_file"
+    control_file = get_payload()
+    try:
+        s3.download_file(bucket, file_key, player_file)
+        file_equality = filecmp.cmp(control_file, player_file)
+        print(file_equality)
+        assert file_equality == True
+    except Exception as e:
+        print(e)
         raise Exception("Payload not detected! Try again.")
