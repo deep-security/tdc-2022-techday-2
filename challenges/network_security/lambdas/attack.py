48b6d9eef40f1e4537699a840a5c608d4579306b
Scott Enand <scott_enand@trendmicro.com>
2022-06-30 15:50:15 -0500

    address PR comments

--- challenges/network_security/lambdas/attack.py
+++ challenges/network_security/lambdas/attack.py
@@ -9,12 +9,12 @@ ec2c = boto3.client("ec2")
 ec2r = boto3.resource("ec2")
 lambdac = boto3.client("lambda")
 
-def sendReq(server='http://2.59.200.0', timeout=2):
+def sendReq(server, timeout=2):
     return curl("curl --connect-timeout {} {}".format(timeout, server).split(' '))
 
-def sendAttack(server='http://2.59.200.0', timeout=2):
-    # return curl("curl --connect-timeout {} -H 'User-Agent: sdvntyer' {}/api/v88".format(timeout, server).split(' '))
-    return curl("curl --connect-timeout {} {}".format(timeout, server).split(' '))  
+def sendAttack(server, timeout=2):
+    return curl("curl --connect-timeout {} -H 'User-Agent: sdvntyer' {}/api/v88".format(timeout, server).split(' '))
+    # return curl("curl --connect-timeout {} {}".format(timeout, server).split(' '))  
 def curl(cmd):
     # Returns True if was able to connect to server
     # AWS Lambda Functions block all ICMP packets, need to use curl instead.
@@ -25,14 +25,14 @@ def curl(cmd):
         if errcode is None:  # No error code means we were able to connect
             return True
         else:
-            return errcode.group(1) != "28"  # errcode 28 means connection timeout
+            return errcode.group(1) != "28"  # errcode 28 means connection timeout, which means Network Security blocked it, or lambda has no internet access
     except Exception as e:
         print(e)
         return None
 
 def handler(event, context):
     msg = ""
-    victim = os.environ.get("VICTIM", "http://2.59.200.0")
+    victim = os.environ.get("VICTIM")
     # req = sendReq(victim)
     req = sendReq('http://www.google.com')
     attack = sendAttack(victim)
