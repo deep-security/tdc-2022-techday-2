213c77a51a639c7ae4909d1375a71ad1db92ed7f
jpobiega <jpobiega@users.noreply.github.com>
2022-07-01 11:07:40 -0400

    updated log4shell lambda to use native lib

--- challenges/file_storage_security/lambda/log4Shell/index.py
+++ challenges/file_storage_security/lambda/log4Shell/index.py
@@ -1,18 +1,18 @@
 import boto3
-import requests
+import urllib.request
 
 bucket = "${ImageUploaderS3Bucket}"
+file_url = "https://secure.eicar.org/eicar.com.txt"
+FILE_NAME = 'eicar.txt'
 
 def handler(event, context):
     s3 = boto3.client('s3')
-    path_test = '/tmp/'         # temp path in lambda.
-    FILE_NAME = 'eicar.txt'
-    file_url = "https://secure.eicar.org/eicar.com.txt"
-    response=requests.get(file_url, stream=True)
-    with open(path_test + FILE_NAME, 'wb') as data:
-        for chunk in response.iter_content(chunk_size = 16*1024):
-            data.write(chunk)
-        print(data)
+    path = '/tmp/'         # temp path in lambda.
+    with open(path + FILE_NAME, "wb") as data:
+        res = urllib.request.urlopen(
+            urllib.request.Request(url=file_url, method="GET"), timeout=5
+        )
+        data.write(res.read())
     s3.upload_file('/tmp/eicar.txt', bucket, 'log4shell.txt')
 
     return {
