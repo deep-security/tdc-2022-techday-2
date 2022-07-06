4f917362a0aa9e5f2c9d62cdbdf04e4547efcfbc
Alex Jackson <contact@ajaxbits.com>
2022-07-06 16:06:34 +0000

    there's no html allowed in gameboard anymore...

--- challenges/file_storage_security/wording/build.sh
+++ challenges/file_storage_security/wording/build.sh
@@ -7,13 +7,12 @@ do
         csplit --prefix='task' --suffix-format='%03d.md' $file /\#\#/ "{*}"  
         mv task000.md  $DESTINATION/title
         sd "# Task" "FSS Task" $DESTINATION/title
-        pandoc --wrap=preserve task001.md > $DESTINATION/description
-        pandoc --wrap=preserve task002.md > $DESTINATION/details
-        pandoc --wrap=preserve task003.md > $DESTINATION/notes
-        pandoc --wrap=preserve task004.md > $DESTINATION/scoring
+        pandoc --wrap=preserve --to plain task001.md > $DESTINATION/description
+        pandoc --wrap=preserve --to plain task002.md > $DESTINATION/details
+        pandoc --wrap=preserve --to plain task003.md > $DESTINATION/notes
+        pandoc --wrap=preserve --to plain task004.md > $DESTINATION/scoring
         cat task005.md task006.md task007.md task008.md > $DESTINATION/hints
-        sd "</*p>" "" $DESTINATION/*
-        sed -i "1d" $DESTINATION/{description,details,notes,scoring}
+        sed -i "1,2d" $DESTINATION/{description,details,notes,scoring}
         rm task*.md
     done
 done
