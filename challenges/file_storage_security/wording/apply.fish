38296db1e4ec657c5549b2bb97644e7d2b227f3f
Alex Jackson <contact@ajaxbits.com>
2022-07-07 17:02:36 +0000

    tweak apply.fish

--- challenges/file_storage_security/wording/apply.fish
+++ challenges/file_storage_security/wording/apply.fish
@@ -12,25 +12,39 @@ function read_confirm
   end
 end
 
-echo "title"
-cat title | pbcopy
-if read_confirm
-    echo "description"
-    cat description | pbcopy
-end
-if read_confirm
-    echo "details"
-    cat details | pbcopy
-end
-if read_confirm
-    echo "notes"
-    cat notes | pbcopy
-end
-if read_confirm
-    echo "scoring"
-    cat scoring | pbcopy
-end
-if read_confirm
-    echo "hints"
-    cat hints
+function apply
+    cd target/$argv
+    echo "title"
+    cat title | pbcopy
+    if read_confirm
+        echo "description"
+        cat description | pbcopy
+    else
+        return 1
+    end
+    if read_confirm
+        echo "details"
+        cat details | pbcopy
+    else
+        return 1
+    end
+    if read_confirm
+        echo "notes"
+        cat notes | pbcopy
+    else
+        return 1
+    end
+    if read_confirm
+        echo "scoring"
+        cat scoring | pbcopy
+    else
+        return 1
+    end
+    if read_confirm
+        echo "hints"
+        cat hints
+    else
+        return 1
+    end
+    cd ../..
 end
