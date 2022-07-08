57ef6c4aa3c5c06c8c2593281b0dda54fdebd6dd
glasscocked <gerald_glasscock@trendmicro.com>
2022-07-07 19:55:25 -0500

    add ssm user

--- challenges/adfs_challenge/scripts/rdp_user.ps1
+++ challenges/adfs_challenge/scripts/rdp_user.ps1
@@ -13,3 +13,6 @@ param (
 $user = New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force $SafeModeAdministratorPassword) -Name $TechDayAdminUser -FullName $TechDayAdminUser -Description "Local Administrator"
  Add-LocalGroupMember -Group administrators -Member $user
  Add-LocalGroupMember -Group "Remote Desktop Users"  -Member $user
+$ssmuser = New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force $SafeModeAdministratorPassword) -Name $SSMUserName -FullName $SSMUserName -Description "ssm-user"
+ Add-LocalGroupMember -Group administrators -Member $user
+ Add-LocalGroupMember -Group "Remote Desktop Users"  -Member $ssmuser 
