b52e2df3ee80e414f43770e9690d6689b1af32cc
glasscocked <gerald_glasscock@trendmicro.com>
2022-07-08 13:23:15 -0500

    add service check with sleep

--- challenges/adfs_challenge/scripts/add_user_domain.ps1
+++ challenges/adfs_challenge/scripts/add_user_domain.ps1
@@ -7,6 +7,26 @@ param (
     [string]$DomainDNSName
 )
 
+$ServiceName = 'adws'
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -ne 'Running')
+{
+
+    Start-Service $ServiceName
+    write-host $arrService.status
+    write-host 'Service starting'
+    Start-Sleep -seconds 60
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
+    {
+        Write-Host 'Service is now Running'
+    }
+
+}
+
+sleep 30
+
 #add user to domain admins
 Add-ADGroupMember -Identity "Domain Admins" -Members $TechDayAdminUser
 Add-ADGroupMember -Identity "Enterprise Admins" -Members $TechDayAdminUser
